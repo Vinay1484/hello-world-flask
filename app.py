fcebcea09102a990b81d1f3ddeb4cb0b79ff7b4d
Vinay1484 <36964071+Vinay1484@users.noreply.github.com>
2022-07-26 21:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import sqlite3
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello ! This is the first project.'
+    conn = sqlite3.connect('database.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("create table if not exists userss (id integer primary key autoincrement, name text, password text)")
+    cursor.execute("insert into userss(name,password) values('admin','admin')")
+    cursor.execute("select * from userss")
+    conn.commit()
+    return json.dumps(cursor.fetchall())
 
 if __name__ == '__main__':
     app.run()
