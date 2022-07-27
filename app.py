9121eee004f6e0239b0d3c19b2ec775cba404b62
Vinay1484 <36964071+Vinay1484@users.noreply.github.com>
2022-07-27 18:54:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, request
-import sqlite3
+from newspaper import Article
+from googletrans import Translator
 import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def news():
-    data = request.json
-    return data["lang"]
+    url = request.form["url"]
+    lang = request.form["lang"]
+    article = Article(url)
+    article.download()
+    article.parse()
+    translator = Translator()
+    text = ""
+    keywords = article.meta_keywords
+    if(lang == "en"):
+        text = article.text
+    else:
+        text = translator.translate(article.text, dest=lang).text
+    return {"text": text, "keywords": keywords}
 
 if __name__ == '__main__':
     app.run()
