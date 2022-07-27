1486d4516d6126cc421ebdaefc334c3a214f2875
Vinay1484 <36964071+Vinay1484@users.noreply.github.com>
2022-07-27 20:03:10 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -7,38 +7,48 @@ import sqlite3
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=10)
+@sched.scheduled_job('interval', minutes=20)
 def youtube():
+    value = 0
     res = requests.get("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=IN&videoCategoryId=10&key=AIzaSyCstEcJGLLkUkilNnRAy4CsRfxfqgVH3R4")
     resJson = res.json()
     body = {}
-    for items in resJson["items"]:
-        items["snippet"]["description"] = ""
-        items["snippet"]["tags"] = ""
-        items["snippet"]["thumbnails"] = ""
-        items["snippet"]["localized"] = ""
+    if("error" not in resJson):
+        for items in resJson["items"]:
+            items["snippet"]["description"] = ""
+            items["snippet"]["tags"] = ""
+            items["snippet"]["thumbnails"] = ""
+            items["snippet"]["localized"] = ""
+        value += 1
     body["trending"] = resJson
     res = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=top%20telugu%20songs&regionCode=IN&type=video&videoCategoryId=10&videoDuration=medium&key=AIzaSyCstEcJGLLkUkilNnRAy4CsRfxfqgVH3R4")
     resJson = res.json()
-    for items in resJson["items"]:
-        items["snippet"]["description"] = ""
-        items["snippet"]["thumbnails"] = ""
+    if("error" not in resJson):
+        for items in resJson["items"]:
+            items["snippet"]["description"] = ""
+            items["snippet"]["thumbnails"] = ""
+        value += 1
     body["telugu"] = resJson
     res = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=top%20hindi%20songs&regionCode=IN&type=video&videoCategoryId=10&videoDuration=medium&key=AIzaSyCstEcJGLLkUkilNnRAy4CsRfxfqgVH3R4")
     resJson = res.json()
-    for items in resJson["items"]:
-        items["snippet"]["description"] = ""
-        items["snippet"]["thumbnails"] = ""
+    if("error" not in resJson):
+        for items in resJson["items"]:
+            items["snippet"]["description"] = ""
+            items["snippet"]["thumbnails"] = ""
+        value += 1
     body["hindi"] = resJson
     res = requests.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=top%20english%20songs&regionCode=IN&type=video&videoCategoryId=10&videoDuration=medium&key=AIzaSyCstEcJGLLkUkilNnRAy4CsRfxfqgVH3R4")
     resJson = res.json()
-    for items in resJson["items"]:
-        items["snippet"]["description"] = ""
-        items["snippet"]["thumbnails"] = ""
+    if("error" not in resJson):
+        for items in resJson["items"]:
+            items["snippet"]["description"] = ""
+            items["snippet"]["thumbnails"] = ""
+        value += 1
     body["english"] = resJson
     body["time"] = time.strftime("%Y-%m-%d %H:%M:%S")
-    res = requests.post("https://my-website-14.000webhostapp.com/", json=body)
-    print(res.status_code)
+    if(value == 4):
+        res = requests.post("https://my-website-14.000webhostapp.com/", json=body)
+        print(res.status_code)
     
     
 @sched.scheduled_job('interval', minutes=15)
