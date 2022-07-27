14e0abd7f3d1bff60d7dc50341c092b0768a05fc
Vinay1484 <36964071+Vinay1484@users.noreply.github.com>
2022-07-27 12:44:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     data = {}
-    with open("youtube.json", "r", encoding="utf-8") as f:
-        data = json.loads(f.read())
-        f.close()
     conn = sqlite3.connect('database.sqlite')
     cursor = conn.cursor()
     cursor.execute("create table if not exists views (views integer primary key autoincrement, time timestamp default current_timestamp)")
     cursor.execute("insert into views default values")
-    cursor.execute("select count(*) from views")
+    cursor.execute("select data from youtube")
     conn.commit()
-    return data
+    return json.dumps(cursor.fetchall())
 
 if __name__ == '__main__':
     app.run()
