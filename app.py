05fd39ca81b9deb37faf11bf4a06973987cc7276
Vinay1484 <36964071+Vinay1484@users.noreply.github.com>
2022-07-27 16:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    data = {}
     conn = sqlite3.connect('database.sqlite')
     cursor = conn.cursor()
     cursor.execute("create table if not exists views (views integer primary key autoincrement, time timestamp default current_timestamp)")
-    cursor.execute("PRAGMA database_list")
+    cursor.execute("insert into views default values")
+    cursor.execute("select count(*) from views")
     conn.commit()
-    conn.close()
-    return json.dumps(cursor.fetchall())
+    return json.dumps({"views" : cursor.fetchall()[0][0]})
 
 if __name__ == '__main__':
     app.run()
